acebe03343de1337711849368e0dd8cca306f8dd
张军 <zhangjun06@qianxin.com>
2019-06-24 18:00:04 +0800

    add thread test

---
+++ hello/src/generate.rs
@@ -0,0 +1,28 @@
+use std::ops::{Generator, GeneratorState};
+
+pub fn generate(){
+    let mut generator = || {
+        let mut curr: u64 = 1;
+        let mut next: u64 = 1;
+        loop {
+            let new_next = curr.checked_add(next);
+
+            if let Some(new_next) = new_next {
+                curr = next;
+                next = new_next;
+                yield curr; // <-- 新的关键字
+            } else {
+                return;
+            }
+        }
+    };
+
+    loop {
+        unsafe {
+            match generator.resume() {
+                GeneratorState::Yielded(v) => println!("{}", v),
+                GeneratorState::Complete(_) => return,
+            }
+        }
+    }
+}
--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,30 +1,8 @@
 #![feature(generators, generator_trait)]
 
-use std::ops::{Generator, GeneratorState};
+//feature需要加到main.rs或者lib.rs
+mod thread;
 
 fn main() {
-    let mut generator = || {
-        let mut curr: u64 = 1;
-        let mut next: u64 = 1;
-        loop {
-            let new_next = curr.checked_add(next);
-
-            if let Some(new_next) = new_next {
-                curr = next;
-                next = new_next;
-                yield curr; // <-- 新的关键字
-            } else {
-                return;
-            }
-        }
-    };
-
-    loop {
-        unsafe {
-            match generator.resume() {
-                GeneratorState::Yielded(v) => println!("{}", v),
-                GeneratorState::Complete(_) => return,
-            }
-        }
-    }
+    thread::run();
 }
