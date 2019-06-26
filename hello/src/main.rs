6f22f0235b84f83eabc5aa9093e3a8c2cb20a9de
张军 <zhangjun06@qianxin.com>
2019-06-26 12:44:04 +0800

    update

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,13 +1,16 @@
 #![feature(generators, generator_trait)]
 
 //feature需要加到main.rs或者lib.rs
-mod thread;
 mod smart;
 mod template;
+mod thread;
+mod traittest;
 
 fn main() {
     thread::lock_run();
     smart::refcell();
 
     template::largest_run();
+
+    traittest::print_person();
 }
---
+++ hello/src/traittest.rs
@@ -0,0 +1,34 @@
+trait Person {
+    fn run(&self) {
+        print!("run\n");
+    }
+
+    fn name(&self) -> String;
+}
+
+fn run<T: Person>(person: &T) {
+    person.run();
+}
+
+fn name(person: &impl Person) {
+    print!("name = {}\n", person.name());
+}
+
+struct Worker {
+    username: String,
+}
+
+impl Person for Worker {
+    fn name(&self) -> String {
+        self.username.clone()
+    }
+}
+
+pub fn print_person() {
+    let zj = Worker {
+        username: String::from("zj"),
+    };
+
+    run(&zj);
+    name(&zj);
+}
