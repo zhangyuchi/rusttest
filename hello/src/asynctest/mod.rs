0dc323589424564a442a414a4ff92885e75b1410
zhangjun <zhangjun06@qianxin.com>
2019-09-05 18:20:28 +0800

    update async test

--- hello/src/asynctest/mod.rs
+++ hello/src/asynctest/mod.rs
@@ -3,9 +3,11 @@ use futures::executor::block_on;
 mod asyncfunc;
 mod asynclamda;
 mod asynctokio;
+mod oldtokio;
 
 pub fn run_test() {
     block_on(asyncfunc::async_main()); // `future` is run and "hello, world!" is printed
     asynclamda::async_main();
     asynctokio::async_main();
+    oldtokio::pool_main();
 }
