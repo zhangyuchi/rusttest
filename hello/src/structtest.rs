40de82049ed9b7e562f3d4ca822cd77b8d9fb173
张军 <zhangjun06@qianxin.com>
2021-04-14 20:29:16 +0800

    update

--- hello/src/structtest.rs
+++ hello/src/structtest.rs
@@ -43,6 +43,41 @@ fn print_animal<T: Debug>(animal: T) {
   print!("animal is {:?}\n", animal);
 }
 
+struct Buffer {
+  buffer: String,
+}
+
+struct Render {
+  current_buffer: Buffer,
+  next_buffer: Buffer,
+}
+//实现结构体 `Render` 的方法
+use std::mem::replace;
+
+impl Render {
+  //实现 update_buffer() 方法，
+  //更新buffer，把 next 更新到 current 中，再更新 next
+  // fn update_buffer(&mut self, buf: String) {
+  //   self.current_buffer = self.next_buffer;
+  //   self.next_buffer = Buffer { buffer: buf };
+  // }
+
+  fn update_buffer(&mut self, buf: String) {
+    self.current_buffer = replace(&mut self.next_buffer, Buffer { buffer: buf });
+  }
+}
+
+impl fmt::Display for Render {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    // Use `self.number` to refer to each positional data point.
+    write!(
+      f,
+      "{{{0},{1}}}",
+      self.current_buffer.buffer, self.next_buffer.buffer
+    )
+  }
+}
+
 pub fn print_info() {
   println!("structtest print_info");
 
@@ -61,7 +96,18 @@ pub fn print_info() {
     },
   };
 
+  let render = Render {
+    current_buffer: Buffer {
+      buffer: String::from("current"),
+    },
+    next_buffer: Buffer {
+      buffer: String::from("next"),
+    },
+  };
+
   print_animal(dog.clone());
   print!("animal is {}\n", dog.animal);
   print!("animal is {:?}\n", dog.animal);
+
+  print!("render is {}\n", render)
 }
